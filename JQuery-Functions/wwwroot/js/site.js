1f1519faf6c2b0d45ba5c62c346ceb74a450bb67
Kirah Cox <kirahanncox@gmail.com>
2025-09-27 18:27:38 -0600

    finished assignment

---
+++ JQuery-Functions/wwwroot/js/site.js
@@ -0,0 +1,21 @@
+﻿
+
+function ToggleVisibility(element){
+    $(element).click(function () {
+        if(element === '#hidebutton'){
+            $(element).fadeOut();
+            $('#showbutton').fadeIn();
+        }
+        else if(element === '#showbutton'){
+            $(element).fadeOut();
+            $('#hidebutton').fadeIn();
+        }
+    })
+
+
+}
+
+$(document).ready(function (){
+    ToggleVisibility('#hidebutton')
+    ToggleVisibility('#showbutton')
+})
